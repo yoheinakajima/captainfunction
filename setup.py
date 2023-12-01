5beead0ca46f520c40f8ecb16b4724a0735f5e5b
Yohei Nakajima <me@yoheinakajima.com>
2023-12-01 15:24:54 -0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='captainfunction',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # Add your dependencies here, e.g., 'requests', 'numpy', etc.
+    ],
+    description='A Python package to dynamically load functions for OpenAI Assistant',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Yohei Nakajima',
+    author_email='info@untapped.vc',
+    url='https://github.com/yoheinakajima',
+    # More metadata can be added here (license, classifiers, etc.)
+)
